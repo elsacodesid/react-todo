3cd7e663aa1127855e523e896795d575096177fd
elsacodesid <elsacodesid@gmail.com>
2024-06-24 09:20:06 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
@@ -9,26 +9,51 @@ function App() {
     "Pick up the kids from school",
   ]);
   const [todoValue, setTodoValue] = useState("");
-  function handleAddTodos(newTodo) {
-    const newTodoList = [...todos, newTodo];
-    setTodos(newTodoList);
-  }
-  function handleDeleteTodo(index){
-    const newTodoList = todos.filter((todo, todoIndex) => {
-      return todoIndex !== index
-    })
-    setTodos(newTodoList)
+  function persistData(newList){
+    localStorage.setItem("todos", JSON.stringify({todos: newList}))
 
   }
-  function handleEditTodo(index){
-    const valueToBeEdited = todos[index]
-    setTodoValue(valueToBeEdited)
-    handleDeleteTodo(index)
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList)
+    setTodos(newTodoList);
   }
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    persistData(newTodoList)
+    setTodos(newTodoList);
+  }
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index];
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
+  }
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos)
+  }, []);
   return (
     <>
-      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
-      <TodoList handleEditTodo={handleEditTodo} todos={todos} handleDeleteTodo={handleDeleteTodo}/>
+      <TodoInput
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+        handleAddTodos={handleAddTodos}
+      />
+      <TodoList
+        handleEditTodo={handleEditTodo}
+        todos={todos}
+        handleDeleteTodo={handleDeleteTodo}
+      />
     </>
   );
 }
